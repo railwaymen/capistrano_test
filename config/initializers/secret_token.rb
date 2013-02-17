671ad131826b9de3e9f1afea583fc05d86ddbed8
marcin <marcin@railwaymen.org>
2013-02-17 13:24:37 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapistranoTest::Application.config.secret_token = '4ff6092b467717d0ff473233a4e9d8ce497e311ecf9ffb12a8ff20af4c76c1bdc8a5276b3c307f55d9ae065a3d77a21766f22918fbf881eaf5b56ac73e2faf9e'
